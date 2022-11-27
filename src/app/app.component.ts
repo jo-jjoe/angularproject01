fb953c4bf12525eb4646c4329379dc3a07c594ea
jo-jjoe <jjoeyahoo@gmail.com>
2022-11-27 10:05:07 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'develop';
+
+
+
+
+passengers = [
+    {
+      id: 1,
+      passengerName: "Freddie Mercury",
+      isVegetarianOrVegan: false,
+      connectedFlights: 2,
+    },
+    {
+      id: 2,
+      passengerName: "Amy Winehouse",
+      isVegetarianOrVegan: true,
+      connectedFlights: 4,
+    },
+      {
+      id: 3,
+      passengerName: "Kurt Cobain",
+      isVegetarianOrVegan: true,
+      connectedFlights: 3,
+    },
+       {
+      id: 3,
+      passengerName: "Michael Jackson",
+      isVegetarianOrVegan: true,
+      connectedFlights: 1,
+    },
+  ];
+
+
+  ngOnInit(){
+   
+
+    const p1=new Promise((resolve,rej)=>{
+      setTimeout(()=>{
+        resolve ("success")
+
+      },4000)
+    })
+    const getdata=async () => {
+      try{
+        console.log("hiiiiii")
+        const data=await p1
+        console.log(data)
+
+
+      }
+      catch{
+
+      }
+      
+    }
+    getdata()
+    
+
+
+  }
 }
+  
+  
